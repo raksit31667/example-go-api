6a5ab814fa8def2f4a7cede883bd3d25104e6da8
raksit31667 <raksit.m@ku.th>
2024-05-06 18:05:42 +0700

    :sparkles: add graceful shutdown

--- main.go
+++ main.go
@@ -1,13 +1,18 @@
 package main
 
 import (
+	"context"
 	"database/sql"
 	"fmt"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
 
 	"github.com/labstack/echo/v4"
+	_ "github.com/lib/pq"
 	"github.com/raksit31667/example-go-api/config"
 	"github.com/raksit31667/example-go-api/router"
-	_ "github.com/lib/pq"
 )
 
 func main() {
@@ -21,7 +26,21 @@ func main() {
 		e.Logger.Fatal(err)
 	}
 	router.RegisterRoutes(e, db)
-
 	address := fmt.Sprintf("%s:%d", config.Server.Hostname, config.Server.Port)
-	e.Logger.Fatal(e.Start(address))
+
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer stop()
+
+	go func() {		
+		if err := e.Start(address); err != nil && err != http.ErrServerClosed {
+			e.Logger.Fatal(err)
+		}
+	}()
+	
+	<-ctx.Done()
+	ctx, cancel := context.WithTimeout(context.Background(), 10 * time.Second)
+	defer cancel()
+	if err := e.Shutdown(ctx); err != nil {
+		e.Logger.Fatal(err)
+	}
 }
